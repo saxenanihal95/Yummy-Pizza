aa462821b7e3057e86d23df6b3b95f1c96a26a1c
Nihal Saxena <saxenanihal95@gmail.com>
2020-05-02 17:50:21 +0530

    Added model, migration and seeders

---
+++ database/seeds/PizzaSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Pizza;
+
+
+class PizzaSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        //
+        $faker = Faker\Factory::create();
+        $json = File::get("database/data/pizza.json");
+        $data = json_decode($json);
+        foreach($data as $obj) {
+            Pizza::create(array(
+                'name' => $obj->name,
+                'image' => $obj->image,
+                'price' => $faker->randomFloat($nbMaxDecimals = NULL, $min = 0, $max = 200)
+            ));
+        }
+    }
+}
